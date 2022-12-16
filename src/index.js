71f32bd9616de6be590b7c5c4fd2d2e198eac781
Jakub Woźny <jakub2787@gmail.com>
2022-12-16 17:36:16 +0100

    added theme

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
